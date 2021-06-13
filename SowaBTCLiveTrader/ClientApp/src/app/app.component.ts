e7351eaa95a9e2d1398a65a68db072ba9381feeb
Vid Stoschitzky <vidstoschitzky@gmail.com>
2021-06-13 15:57:00 +0200

    Added BTC service to the appComponent and started the connection.

--- SowaBTCLiveTrader/ClientApp/src/app/app.component.ts
+++ SowaBTCLiveTrader/ClientApp/src/app/app.component.ts
@@ -1,9 +1,16 @@
-import { Component } from '@angular/core';
+import { BtcDataService } from "./btc-data.service";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html'
+  selector: "app-root",
+  templateUrl: "./app.component.html",
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  constructor(private _btcDataService: BtcDataService) {}
+
+  ngOnInit() {
+    // Start signalR connection and start listening for btc data
+    this._btcDataService.startConnection();
+    this._btcDataService.addTransferChartDataListener();
+  }
 }
