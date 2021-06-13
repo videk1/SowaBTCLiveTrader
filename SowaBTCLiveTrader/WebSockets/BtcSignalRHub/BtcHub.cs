6d2f0672b710f6597a289732b687d9e9af61eae0
Vid Stoschitzky <vidstoschitzky@gmail.com>
2021-06-13 15:54:05 +0200

    Added signalR hub so I can send the data to the client everytime I get some from Bitstamp.

---
+++ SowaBTCLiveTrader/WebSockets/BtcSignalRHub/BtcHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SowaBTCLiveTrader.WebSockets.BtcSignalRHub
+{
+    public class BtcHub : Hub
+    {
+        public Task SendMessage(string user, string message)
+        {
+            return Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        #region OnConnectedAsync
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnConnectedAsync();
+        }
+        #endregion
+
+        #region OnDisconnectedAsync
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "SignalR Users");
+            await base.OnDisconnectedAsync(exception);
+        }
+        #endregion
+    }
+}
